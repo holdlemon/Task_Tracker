3d125881b8a529072a456701805db930a77c3938
holdlemon <d.vajov2010@yandex.ru>
2025-03-02 22:42:07 +0300

    Настроена API документация

--- config/settings.py
+++ config/settings.py
@@ -41,8 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_yasg',
     'rest_framework',
-    'tracker'
+    'tracker',
 ]
 
 MIDDLEWARE = [
