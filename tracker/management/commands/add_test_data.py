b0787dae9452b939b344bdd6dfda4bb31eeda04e
holdlemon <d.vajov2010@yandex.ru>
2025-03-04 23:23:07 +0300

    Добавлена кастомная команда для загрузки тестовых данных

---
+++ tracker/management/commands/add_test_data.py
@@ -0,0 +1,20 @@
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+
+from tracker.models import Employee, Task
+
+
+class Command(BaseCommand):
+    help = "Добавление тестовых данных из фикстур"
+
+    def handle(self, *args, **kwargs):
+        # Удаляем существующие записи
+        Employee.objects.all().delete()
+        Task.objects.all().delete()
+
+        # Загружаем данные из фикстур
+        call_command("loaddata", "employee_fixture.json", format="json")
+        self.stdout.write(self.style.SUCCESS("Сотрудники загружены из фикстур успешно"))
+
+        call_command("loaddata", "task_fixture.json", format="json")
+        self.stdout.write(self.style.SUCCESS("Задачи загружены из фикстур успешно"))
